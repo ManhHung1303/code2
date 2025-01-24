642ad01e601bed0730a1004faf7b2d398662d4b9
ManhHung1303 <manhhunghm13032005@gmail.com>
2025-01-24 09:09:48 +0700

    code2
    upload lần 1

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+  introTitle: "Anh có lời muốn nói muốn gửi đến Châu Linh 😘😘",
+  introDesc: ` Lần này anh may mắn  đã tìm được đúng cô gái thiên bình t10 thông minh, xinh gái được ví như bạch nguyệt quang của đời anh rùi `,
+  btnIntro: "Đặc biệt hơn ở phía sau nè ",
+  title: " Anh thực sự rất thích em và yêu em  😙",
+  desc: "Châu Linh ơi, em có thể cho anh 1 cơ hội được làm người yêu em đồng hành cùng em  nka 😘😘   ",
+  btnYes: "Em đồng ý  <33",
+  btnNo: "Không ạ :3",
+  question:
+    "Trên thế giới hơn 7 tỉ người em là người đặc biệt nhất đối với anh <3",
+  btnReply: "Gửi cho anh <3",
+  reply: "ok a",
+  mess: "Yêu em nhiều lém  😘😘",
+  messDesc: "Em đồng ý , anh có thể nguyện mời em một bữa nhé ",
+  btnAccept: "Okiiiii lun <3",
+  messLink: "https://www.facebook.com/messages/e2ee/t/9084197011670398", //link mess của các bạn. VD: https://m.me/nam.nodemy
+};
